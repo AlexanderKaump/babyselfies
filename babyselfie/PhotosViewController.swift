76cc74879df4ebed5c14a73215fbd0f7836b45cf
Adam Kaump <adam.kaump@gmail.com>
2016-08-06 15:27:32 -0400

    Added photos grid

--- babyselfie/PhotosViewController.swift
+++ babyselfie/PhotosViewController.swift
@@ -8,12 +8,40 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    let photoNames = ["selfie1", "selfie2", "selfie3", "selfie4", "selfie5"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBarHidden = false
+        
+        let width = self.view.frame.size.width/4
+        (self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout).itemSize = CGSize(width: width, height: width)
+    }
+
+}
+
+extension PhotosViewController {
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
     }
     
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photoNames.count
+    }
     
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PhotoCell", forIndexPath: indexPath) as! PhotoCell
+        let name = self.photoNames[indexPath.row]
+        cell.imageView.image = UIImage(named: name)
+        return cell
+    }
+}
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
 }
